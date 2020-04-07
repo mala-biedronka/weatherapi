a590dfb51282703f5bf241b3a8495b8fb2948318
Iryna Predborska <iryna.predborska@gmail.com>
2020-04-07 16:21:59 +0200

    add get and send request

--- index.js
+++ index.js
@@ -13,11 +13,15 @@ app.get("/", function (req, res) {
         const weatherData = JSON.parse(data);
         const temp = weatherData.main.temp;
         const descript = weatherData.weather[0].description;
-            console.log(descript);
+        const weatherIcon = weatherData.weather[0].icon;
+        const imageURL = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+        res.write("<h1>The temperature in London is " + temp + " degrees Celcius.</h1>");
+        res.write("<p>The weather is currently " + descript + "</p>");
+        res.write("<img src=" + imageURL + ">");
+        res.send();
+
         });
     });
-
-    res.send("Server is up and running");
 });
 
 app.listen(3000, function () {
