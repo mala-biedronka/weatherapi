af6ffa5f40c54a4f9d942631079cb74cd3e77dc5
Iryna Predborska <iryna.predborska@gmail.com>
2020-04-14 10:30:59 +0200

    create a post request and putting all together

--- index.js
+++ index.js
@@ -7,25 +7,35 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get("/", function (req, res) {
-    const externalUrl = "https://api.openweathermap.org/data/2.5/weather?q=Wroclaw&appid=68bad42650b6de20396b2ba0c4520d6c&units=metric";
 
-    https.get(externalUrl, function (response) {
-        console.log(response.statusCode);
-        response.on("data", function (data) {
-            //Parse data into JavaScript Object
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const descript = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    res.sendFile(__dirname + "/index.html");
 
-            res.write("<p>The weather is currently is " + descript +"</p>");
-            res.write("<h1>The current temperature in Wroclaw is " + temp + " degrees Celcius.</h1>");
-            res.write("<img src=" + imageURL + ">");
-            res.send();
-        })
-    });
+    app.post("/", function (req, res) {
+        const query = req.body.cityName;
+        const apiKey = "68bad42650b6de20396b2ba0c4520d6c";
+        const units = "metric";
+        const externalUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units;
+
+        https.get(externalUrl, function (response) {
+            console.log(response.statusCode);
+            response.on("data", function (data) {
+                //Parse data into JavaScript Object
+                const weatherData = JSON.parse(data);
+                const temp = weatherData.main.temp;
+                const descript = weatherData.weather[0].description;
+                const icon = weatherData.weather[0].icon;
+                const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+                res.write("<p>The weather currently is " + descript + "</p>");
+                res.write("<h1>The current temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
+                res.write("<img src=" + imageURL + ">");
+                res.send();
+            })
+        });
+    })
 })
+
+
 
 app.listen(3000 , function () {
     console.log("Server is running on the port 3000.")
